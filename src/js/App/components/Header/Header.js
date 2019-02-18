e6c1b4cdbc894758089cf49f1ba65aea7a53c3a0
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2019-02-18 10:10:00 +0100

    Create Header component

---
+++ src/js/App/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import fonts from '../../styles/fonts';
+import colors from '../../styles/colors';
+
+const StyledHeader = styled.header`
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-size: ${fonts.sizes.xxl};
+  font-family: ${fonts.Starjout};
+  font-weight: normal;
+  color: ${colors.yellow};
+
+  small {
+    font-family: ${fonts.Starjhol};
+    font-size: ${fonts.sizes.xl};
+    display: block;
+  }
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <H1>
+      Star Wars<small>Characters</small>
+    </H1>
+  </StyledHeader>
+);
+export default Header;
