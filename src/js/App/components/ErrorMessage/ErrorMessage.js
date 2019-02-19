66412e3e853032032f8b0f96f4de3ecd405a09d6
Ivan Aguilar Martin <iaguilarmartin@MacBook-Pro-de-Barbara.local>
2019-02-19 15:23:38 +0000

    Fix ErrorMessage format

--- src/js/App/components/ErrorMessage/ErrorMessage.js
+++ src/js/App/components/ErrorMessage/ErrorMessage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+import fonts from '../../styles/fonts';
+import { space } from '../../styles/spacing';
+
+const Section = styled.section`
+  text-align: center;
+  margin: ${space.x3} 0;
+  font-family: ${fonts.Starjedi};
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: ${space.x2} auto;
+`;
 
 const ErrorMessage = () => (
-  <div>
-    <img alt="" src="/assets/images/porg.png" />
-  </div>
+  <Section>
+    <Image alt="" src="/assets/images/porg.png" />
+    ooopss something went wrong...
+  </Section>
 );
 
 export default ErrorMessage;
